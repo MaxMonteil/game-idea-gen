f4b50d043a5082c09766dbd9091b2c601253f7b3
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-11 23:32:20 +0300

    Create basic idea fragment logic

---
+++ src/components/AppIdeaFragment.vue
@@ -0,0 +1,23 @@
+<template>
+  <span @click="locked = !locked">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'AppIdeaFragment',
+  props: {
+    word: String
+  },
+  data () {
+    return {
+      locked: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
