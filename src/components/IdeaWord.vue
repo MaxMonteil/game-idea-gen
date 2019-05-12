debf1b24318943dee503e55e318ee075520e1185
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-12 17:37:23 +0300

    Separate word and fragment handling

---
+++ src/components/IdeaWord.vue
@@ -0,0 +1,44 @@
+<template>
+  <span @click="lockWord">
+    {{ displayWord }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'IdeaWord',
+  props: {
+    word: String
+  },
+  data () {
+    return {
+      displayWord: this.word,
+      cachedWord: '',
+      locked: false
+    }
+  },
+  methods: {
+    lockWord () {
+      // Locking word
+      if (!this.locked) {
+        this.cachedWord = this.word
+        this.locked = true
+      } else { // Unlocking word
+        this.locked = false
+      }
+    }
+  },
+  watch: {
+    word (newWord) {
+      if (this.locked) {
+        this.displayWord = this.cachedWord
+      } else {
+        this.displayWord = newWord
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
