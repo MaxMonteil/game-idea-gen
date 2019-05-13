edb57bc3049b3be865d45bc05d27a608daa57d15
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-14 01:29:15 +0300

    Remove svg in favor of opacity to avoid licenses

--- src/components/IdeaWord.vue
+++ src/components/IdeaWord.vue
@@ -1,25 +1,6 @@
 <template>
-  <span class="idea-word" :class="{ visible: word }">
-    <span @click="lockWord" class="display-word">
-      {{ displayWord }}
-    </span>
-    <svg
-      v-if="word && locked"
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fas"
-      data-icon="lock"
-      class="svg-inline--fa fa-lock fa-w-14"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512"
-      width="10"
-    >
-      <path
-        fill="currentColor"
-        d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
-      ></path>
-    </svg>
+  <span @click="lockWord" :class="{ locked: locked }" class="display-word">
+    {{ displayWord }}
   </span>
 </template>
 
@@ -60,13 +41,15 @@ export default {
 </script>
 
 <style scoped>
-.idea-word {
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
+.locked {
+  opacity: 0.5;
 }
 
-.visible {
-  padding: 0 0.1em;
+.display-word {
+  text-decoration: underline;
+}
+
+.display-word:hover {
+  opacity: 0.5;
 }
 </style>
