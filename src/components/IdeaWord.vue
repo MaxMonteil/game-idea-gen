67eea3bc91e2fbc19abc367fa083fb7922851af9
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-13 22:10:56 +0300

    Add little lock icon for locked words

--- src/components/IdeaWord.vue
+++ src/components/IdeaWord.vue
@@ -1,6 +1,25 @@
 <template>
-  <span @click="lockWord">
-    {{ displayWord }}
+  <span class="idea-word" :class="{ visible: word }">
+    <span @click="lockWord" class="display-word">
+      {{ displayWord }}
+    </span>
+    <svg
+      v-if="word && locked"
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fas"
+      data-icon="lock"
+      class="svg-inline--fa fa-lock fa-w-14"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+      width="10"
+    >
+      <path
+        fill="currentColor"
+        d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
+      ></path>
+    </svg>
   </span>
 </template>
 
@@ -30,6 +49,7 @@ export default {
   },
   watch: {
     word (newWord) {
+      console.log('hit')
       if (this.locked) {
         this.displayWord = this.cachedWord
       } else {
@@ -40,5 +60,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.idea-word {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.visible {
+  padding: 0 0.1em;
+}
 </style>
