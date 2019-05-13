e94ec56433925dc660a6b3b999f9ba216e0c7cc7
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-13 22:49:53 +0300

    Prevent useless render, use proper value for key

--- src/components/IdeaFragment.vue
+++ src/components/IdeaFragment.vue
@@ -1,15 +1,15 @@
 <template>
   <span>
-    <span v-for="(word, i) in fragment.words" :key="word.noun">
+    <span v-for="(word, i) in fragment.words" :key="i">
       <!-- Word separators  -->
       <span v-if="fragment.joiner === '/' && fragment.words.length > 1 && i > 0">/</span>
       <span v-if="fragment.joiner === ',' && fragment.words.length > 1 && i > 0">,</span>
-      <span v-if="fragment.joiner === ',' && fragment.words.length > 1 && i === fragment.words.length - 1"> and</span>
+      <span v-if="fragment.joiner === ',' && fragment.words.length > 1 && i === fragment.words.length - 1"> and </span>
 
       <!-- Fragment words  -->
-      <idea-word :word="word.verb" />
-      <idea-word :word="word.adj" />
-      <idea-word :word="word.noun" />
+      <idea-word v-if="word.verb" :word="word.verb" />
+      <idea-word v-if="word.adj" :word="word.adj" />
+      <idea-word v-if="word.noun" :word="word.noun" />
     </span>
   </span>
 </template>
